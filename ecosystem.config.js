2a02cac22512e093f93c45d9b888fea29b1f9c57
hewei <385341490@qq.com>
2022-06-19 03:16:17 +0800

    配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
    apps : [{   //数组里的每个对象就是一个进程的属性信息
-    name:"yadea",
-    script: './bin/www',
+    name:"yadea",//进程的名字
+     script: './bin/www',//要执行的地址
     instances: 1,  //开启的进程数
     autorestart: true, //自动重启
     watch: 'true',     //开启监听，监听到文件变化，就会重启
@@ -26,21 +26,19 @@ module.exports = {
   // }
 ],
 
-  deploy : {
+  deploy : {//自动化部署的配置
     production : {
-      user : 'administrator',
-      host : ['8.134.54.84'],
-      ref  : 'origin/master',
+      user : 'administrator',//服务器登录的user名
+      host : ['8.134.54.84'],//服务器地址
+      ref  : 'origin/master',//git上的分支，启动后会将仓库中的文件拉取到远程服务器上github
       repo : 'git@github.com:sam13928996193/yadea-server.git',
       path : '/User/sam/www/yadea',  //放在服务上的路径
-      ssh_options:[// 设置密钥的检测，
-                  "StrictHostKeyChecking=no",
-                  ],
-      'pre-deploy-local': '',
+      ssh_options:"StrictHostKeyChecking=no",// 设置密钥的检测，
+      // 部署后会自动运行的命令
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       // 'pre-setup': ''
        "env": {
-        "NODE_ENV": 'production'
+        "NODE_ENV": "production"
        },
     }
   }
